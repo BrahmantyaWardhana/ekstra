33c6fefd120928f6532acbf873bfcb1ff8d52f48
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-06-25 14:37:24 -0500

    test vercel commit

--- code/src/app/page.tsx
+++ code/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
 		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
 				<h1 className="font-extrabold text-5xl text-white tracking-tight sm:text-[5rem]">
-					Test <span className="text-[hsl(280,100%,70%)]">App</span> Commit
+					Test <span className="text-[hsl(280,100%,70%)]">Vercel</span> Commit
 				</h1>
 			</div>
 		</main>
