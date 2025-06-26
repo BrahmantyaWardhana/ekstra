bf5ddd2dd3cbed325376bc3755e151001066aa70
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-06-26 18:33:32 -0500

    update drizzle table creation prefix

--- code/src/server/db/schema.ts
+++ code/src/server/db/schema.ts
@@ -8,7 +8,7 @@ import type { AdapterAccount } from "next-auth/adapters";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `code_${name}`);
+export const createTable = pgTableCreator((name) => `ekstra_${name}`);
 
 export const posts = createTable(
 	"post",
