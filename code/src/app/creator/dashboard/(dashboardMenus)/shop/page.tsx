86f368b513f7ca873d0a6f66f0e034dcd38a6ef6
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-07-21 13:43:49 -0500

    updated middleware to properly secure creator routes (ai generated)

--- code/src/app/creator/dashboard/(dashboardMenus)/shop/page.tsx
+++ code/src/app/creator/dashboard/(dashboardMenus)/shop/page.tsx
@@ -1,7 +1,7 @@
 export default function CreatorDashboardShop() {
   return (
     <main className="p-6">    
-      <div className="w-1/2 mx-auto">
+      <div className="w-2/3 mx-auto">
         Creator Dashboard Shop Page
       </div>
     </main>
