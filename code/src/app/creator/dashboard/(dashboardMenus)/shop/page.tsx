f4156c6bc649d5398d40976265c703120006b733
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-08-18 07:42:59 +0700

    fixed inconsistent styling for creator/shop

--- code/src/app/creator/dashboard/(dashboardMenus)/shop/page.tsx
+++ code/src/app/creator/dashboard/(dashboardMenus)/shop/page.tsx
@@ -4,7 +4,7 @@ import CreatorShop from "~/components/CreatorShop";
 export default function CreatorDashboardShop() {
   return (
     <main className="p-6">    
-      <div className="w-2/3 mx-auto">
+      <div className="w-1/2 mx-auto">
         <CreatorShop />
       </div>
     </main>
