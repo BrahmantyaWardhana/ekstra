1cec71ee25cd43c40748bdc120b0fa22d9479096
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2025-08-24 19:52:38 +0700

    bandaid oauth error fix

--- code/src/middleware.ts
+++ code/src/middleware.ts
@@ -1,31 +1,36 @@
 import { auth } from "~/server/auth";
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-const USER_ROUTES = ["/user/"]
-const CREATOR_ROUTES = ["/creator/"]
-
+const USER_ROUTES = ["/user/"];
+const CREATOR_ROUTES = ["/creator/"];
 
 export async function middleware(request: NextRequest) {
-  const { nextUrl } = request
-  const pathname = nextUrl.pathname; 
-  
-  const session = await auth()
-  const isAuthenticated = !!session?.user
-  const isCreator = !!session?.user.creatorPageId
+  const { nextUrl } = request;
+  const pathname = nextUrl.pathname;
 
-  const isNotPublicRoute = USER_ROUTES.some((route: any) => pathname.startsWith(route));
-  const isCreatorRoute = CREATOR_ROUTES.some((route: any) => pathname.startsWith(route));
+  if (pathname === "/api/auth/error" && nextUrl.searchParams.get("error") === "Configuration") {
+    return NextResponse.redirect(new URL("/", nextUrl));
+  }
+
+  const session = await auth();
+  const isAuthenticated = !!session?.user;
+  const isCreator = !!session?.user?.creatorPageId;
+
+  const isNotPublicRoute = USER_ROUTES.some(route => pathname.startsWith(route));
+  const isCreatorRoute = CREATOR_ROUTES.some(route => pathname.startsWith(route));
 
   if (!isAuthenticated && isNotPublicRoute) {
-    return NextResponse.redirect(new URL("/login", nextUrl))
+    return NextResponse.redirect(new URL("/login", nextUrl));
   }
 
   if (isCreatorRoute && !isCreator) {
     return NextResponse.redirect(new URL("/creatorsetup/", nextUrl));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
